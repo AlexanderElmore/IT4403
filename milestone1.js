2bf043037c9e112102f2d09a090ca3efa2a6d0a7
AlexanderElmore <AlexanderElmore@outlook.com>
2025-06-25 21:11:18 -0600

    Milestone 1 Page Update

---
+++ milestone1.js
@@ -0,0 +1,15 @@
+function renderToHTML(data) {
+    const container = document.getElementById("books-list");
+    container.innerHTML = data.map(book => `
+        <div class="book">
+            <h3>${book.title}
+        `).join('');
+}
+
+fetch('google-books-book.json')
+    .then(response => response.json())
+    .then(data => renderToHTML(data))
+    .catch(error => console.error("Error loading JSON:", error));
+
+
+renderToHTML(books)

629e62d9ffbf81537dd9fb11f55b4a13980b3e24
AlexanderElmore <AlexanderElmore@outlook.com>
2025-06-25 21:44:12 -0600

    Milestone 1 Page Update 4

--- milestone1.js
+++ milestone1.js
@@ -2,11 +2,26 @@ function renderToHTML(data) {
     const container = document.getElementById("books-list");
     container.innerHTML = data.map(book => `
         <div class="book">
-            <h3>${book.title}
+            <div>${book.volumeInfo.title}</div>
+        </div>
         `).join('');
 }
 
 fetch('google-books-book.json')
     .then(response => response.json())
     .then(data => renderToHTML(data))
+    .catch(error => console.error("Error loading JSON:", error));
+
+function renderToHTML2(data) {
+    const container = document.getElementById("search-list");
+    container.innerHTML = data.map(book => `
+        <div class="book">
+            <div>${book.volumeInfo.title}</div>
+        </div>
+        `).join('');
+}
+
+fetch('google-books-search.json')
+    .then(response => response.json())
+    .then(data => renderToHTML2(data.items))
     .catch(error => console.error("Error loading JSON:", error));

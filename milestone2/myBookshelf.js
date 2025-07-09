222a4e4912cf0c4cb2495318fb90a2a590bc6b33
AlexanderElmore <AlexanderElmore@outlook.com>
2025-07-09 00:03:25 -0600

    Milestone 3 Update 5

--- milestone2/myBookshelf.js
+++ milestone2/myBookshelf.js
@@ -2,7 +2,6 @@ $(async function() {
 
 let userId = '101044350015130496054';
 let shelfId = 0;
-let apiKey = 'AIzaSyDF2KU0miVRTUPzhKVmX2cXJCGQv5Z1Kb8';
 
 let url = `https://www.googleapis.com/books/v1/users/${userId}` + 
         `/bookshelves/${shelfId}/volumes` + 
